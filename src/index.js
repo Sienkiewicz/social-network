62cbf1c70c8c9d9acd643bd3ffd758d55a46f70a
Sienkiewicz <peter@sinkevich.com.ua>
2020-10-01 21:38:52 +0200

    1

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import './index.scss';
 import App from './App';
 import store from './redux/redux-store';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<HashRouter>
 			<Provider store={store}>
 				<App />
 			</Provider>
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
