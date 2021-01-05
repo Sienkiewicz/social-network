b4e68b99ef3809b7ecdc244bb2e5be249b35f887
Sienkiewicz <peter@sinkevich.com.ua>
2021-01-05 11:15:05 +0100

    search

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,78 +1,92 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import {
-    follow,
-    unfollow,
-    getUsers,
-} from '../../redux/users-reducer';
-import Users from './Users';
-import Preloader from '../common/preloaders/Preloader';
-import { AppStateType } from '../../redux/redux-store';
-import { UserType } from '../common/Types'
+import React, { FC, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getUsers, FilterType } from '../../redux/users-reducer'
+import Users from './Users'
+import Preloader from '../common/preloaders/Preloader'
+import { AppStateType } from '../../redux/redux-store'
+import { useHistory } from 'react-router-dom'
 
-class UsersAPIComponent extends React.Component<
-  MapDispatchPropsType & MapStatePropsType
-> {
-  componentDidMount() {
-    this.props.getUsers(this.props.currentPage, this.props.pageSize)
+  type QueryParamsType = {
+    term?: string
+    friend?: string
+    page?: string
   }
 
-  onPageChanged = (pageNumber: number) => {
-    this.props.getUsers(pageNumber, this.props.pageSize)
+const UsersContainer: FC = () => {
+  const queryString = require('query-string')
+  const history = useHistory()
+  const pageSize = useSelector(
+    (state: AppStateType) => state.usersPage.pageSize
+  )
+  const isFetching = useSelector(
+    (state: AppStateType) => state.usersPage.isFetching
+  )
+  const currentPage = useSelector(
+    (state: AppStateType) => state.usersPage.currentPage
+  )
+  const totalUsersCount = useSelector(
+    (state: AppStateType) => state.usersPage.totalUsersCount
+  )
+  const users = useSelector((state: AppStateType) => state.usersPage.users)
+  const filter = useSelector((state: AppStateType) => state.usersPage.filter)
+  const followingInProgress = useSelector(
+    (state: AppStateType) => state.usersPage.followingInProgress
+  )
+
+  const onPageChanged = (pageNumber: number) => {
+    dispatch(getUsers(pageNumber, pageSize, filter))
   }
 
-  render() {
-    return (
-      <>
-        {this.props.isFetching ? <Preloader /> : null}
-        <Users
-          totalUsersCount={this.props.totalUsersCount}
-          pageSize={this.props.pageSize}
-          currentPage={this.props.currentPage}
-          onPageChanged={this.onPageChanged}
-          users={this.props.users}
-          follow={this.props.follow}
-          unfollow={this.props.unfollow}
-          followingInProgress={this.props.followingInProgress}
-        />
-      </>
-    )
+  const onFilterChanged = (filter: FilterType) => {
+    dispatch(getUsers(1, pageSize, filter))
   }
+  const dispatch = useDispatch()
+
+useEffect(() => {
+const parsed = queryString.parse(history.location.search) as QueryParamsType
+
+let actualPage = currentPage
+let actualFilter = filter
+if(parsed.page) actualPage = +parsed.page
+if(parsed.term) actualFilter = {...actualFilter, term: parsed.term}
+if(parsed.friend) actualFilter = {
+  ...actualFilter,
+  friend: parsed.friend === 'null' ? null : parsed.friend === 'true' ? true : false
+}
+dispatch(getUsers(actualPage, pageSize, actualFilter))
+  }, [])
+
+  useEffect(() => {
+    const query: QueryParamsType = {}
+    if(filter.term) query.term = filter.term
+    if(filter.friend !== null) query.friend = String(filter.friend)
+    if(currentPage !== 1) query.page = String(currentPage)
+
+    
+    history.push({
+      pathname: '/users',
+      search: queryString.stringify(query)
+    })
+  }, [filter, currentPage])
+
+  // useEffect(() => {
+  //   dispatch(getUsers(currentPage, pageSize, filter))
+  // }, [currentPage, filter])
+
+  return (
+    <>
+      {isFetching ? <Preloader /> : null}
+      <Users
+        totalUsersCount={totalUsersCount}
+        pageSize={pageSize}
+        currentPage={currentPage}
+        onPageChanged={onPageChanged}
+        onFilterChanged={onFilterChanged}
+        users={users}
+        followingInProgress={followingInProgress}
+      />
+    </>
+  )
 }
 
-let mapStateToProps = (state: AppStateType): MapStatePropsType => {
-    return {
-        users: state.usersPage.users,
-        pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage,
-        isFetching: state.usersPage.isFetching,
-        followingInProgress: state.usersPage.followingInProgress,
-    };
-};
-
-const UsersContainer = connect<MapStatePropsType, MapDispatchPropsType, {}, AppStateType>(mapStateToProps, {
-    follow,
-    unfollow,
-    getUsers,
-})(UsersAPIComponent);
-
-export default UsersContainer;
-
-// * от костанты до экспорт дэфолт можно записать одной строкой
-// ? export default connect((mapStateToProps, mapDispatchToProps)(Users)
-
-type MapStatePropsType = {
-  currentPage: number
-  pageSize: number
-  isFetching: boolean
-  totalUsersCount: number
-  users: UserType[]
-  followingInProgress: number[]
-}
-
-type MapDispatchPropsType = {
-  getUsers: (currentPage: number, pageSize: number) => void
-  unfollow: (userId: number) => void
-  follow: (userId: number) => void
-}
+export default UsersContainer
